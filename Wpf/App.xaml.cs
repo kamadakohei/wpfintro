620cd0f63c4b4c1ea415670d27f07e233f0a6e0e
kamada <kamada.math@gmail.com>
2020-07-30 22:38:36 +0900

    first commit

---
+++ Wpf/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf.ViewModels;
+using Wpf.Views;
+
+namespace Wpf
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var w = new MainView();
+            var vm = new MainViewModel();
+
+            w.DataContext = vm;
+
+            w.Show();
+        }
+    }
+}
---
+++ Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Wpf.ViewModels
+{
+    internal class MainViewModel : NotificationObject
+    {
+        private String _upperString;
+
+        /// <summary>
+        /// すべての大文字に変換した文字列を取得します。
+        /// </summary>
+        public string UpperString
+        {
+            get { return this._upperString; }
+            private set
+            {
+                SetProperty(ref this._upperString, value);}
+            }
+
+        private string _inputString;
+
+        public string InputString
+        {
+            get { return this._inputString; }
+            set
+            {
+                if (SetProperty(ref this._inputString, value))
+                {
+                    // 入力された文字列を大文字に変換します。
+                    this.UpperString = this._inputString.ToUpper();
+
+                    //　出力ウィンドウに結果を表示します。
+                    System.Diagnostics.Debug.WriteLine("UpperString=" + this.UpperString);
+                }
+            }
+        }
+
+        private DelegateCommand _clearCommand;
+
+
+        public DelegateCommand ClearCommand
+        {
+            get
+            {
+                if(this._clearCommand == null)
+                {
+                    this._clearCommand = new DelegateCommand(_ =>
+                    {
+                        this.InputString = "";
+                    });
+                }
+                return this._clearCommand;
+            }
+        }
+
+    }
+}
+
