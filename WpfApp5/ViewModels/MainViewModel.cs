4c6b49b73462cb50b058a6e8c810dd2b5f7bb28d
kamada <kamada.math@gmail.com>
2020-07-30 23:43:34 +0900

    add menu and status bar add open logic

---
+++ WpfApp5/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace WpfApp5.ViewModels
+{
+    /// <summary>
+    /// MainWindowウィンドウに対するデータコンテキストを表します。
+    /// </summary>
+    internal class MainViewModel : NotificationObject
+    {
+        private DelegateCommand _openFileCommand;
+
+        /// <summary>
+        /// ファイルを開くコマンドを取得します。
+        /// </summary>
+        public DelegateCommand OpenFileCommand
+        {
+            get
+            {
+                return this._openFileCommand ?? (this._openFileCommand = new DelegateCommand(
+                _ =>
+                {
+                    System.Diagnostics.Debug.WriteLine("ファイルを開きます");
+                }));
+            }
+        }
+
+    }
+}
--- WpfApp5/Views/MainWindow.xaml.cs
+++ WpfApp5/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp5
+namespace WpfApp5.Views
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
